4e06e66658be4f0314ac24b87772f78fe3de6d50
Chandan Gowda <chandansuresh007@gmail.com>
2021-06-24 14:15:30 +0530

    Speech Recognition implemented

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 import time
 from path import *
+import speech_recognition as sr
+import pyaudio
 
 def waitUntilVisibility(path):
     wait = WebDriverWait(driver, 1000)  #waiting for expected conditions
@@ -44,9 +46,46 @@ def markAttendence(message,chatbutton,textarea,sendbutton):
     driver.find_element_by_xpath(sendbutton).click()
     print(9)
 
+    waitUntilVisibility(chatbutton)
+    driver.find_element_by_xpath(chatbutton).click()
+    print(10)
+
+def recognizeVoice():
+    p = pyaudio.PyAudio()
+    for i in range(p.get_device_count()):
+        dev = p.get_device_info_by_index(i)
+        if (dev['name'] == 'Stereo Mix (Realtek(R) Audio)' and dev['hostApi'] == 0):
+            dev_index = dev['index']
+            print('dev_index', dev_index)
+
+    while(True):
+
+        r = sr.Recognizer()
+        print(1)
+
+
+        with sr.Microphone(device_index=dev_index) as source:
+
+            audio = r.listen(source)
+            print(2)
+            MyText = r.recognize_google(audio, language = 'en-IN', show_all = True)
+            print(3)
+            text = MyText.get('alternative')
+            a = text[0]
+            recognizedVoice = a['transcript'].lower()
+            print(recognizedVoice)
+
+            if 'chandan' in recognizedVoice:
+                markAttendence(message,chatbutton,textarea,sendbutton)
+            else:
+                pass
+
 
 
 meetcode = str(input("Input Meet Code >>  ")) #input from user
+
+
+
 message = str(input("Message >>  "))
 
 
@@ -60,9 +99,14 @@ wait = WebDriverWait(driver, 1000)
 
 
 goToUrl(meetcode)
+time.sleep(5)
 disableCamMic(mic,cam)
+time.sleep(5)
 join(joinbutton)
+time.sleep(5)
 markAttendence(message,chatbutton,textarea,sendbutton)
+time.sleep(5)
+recognizeVoice()
 
 
 
--- speech.py
+++ speech.py
@@ -1,28 +1,37 @@
-from selenium import webdriver 
-from time import sleep 
-import speech_recognition as sr 
+from selenium import webdriver
+from time import sleep
+import speech_recognition as sr
 import pyaudio
+from main import *
+
+def recognizeVoice():
+    p = pyaudio.PyAudio()
+    for i in range(p.get_device_count()):
+        dev = p.get_device_info_by_index(i)
+        if (dev['name'] == 'Stereo Mix (Realtek(R) Audio)' and dev['hostApi'] == 0):
+            dev_index = dev['index']
+            print('dev_index', dev_index)
+
+    while(True):
+
+        r = sr.Recognizer()
+        print(1)
 
 
-# p = pyaudio.PyAudio()
-# for i in range(p.get_device_count()):
-#     dev = p.get_device_info_by_index(i)
-#     if (dev['name'] == 'Stereo Mix (Realtek(R) Audio)' and dev['hostApi'] == 0):
-#         dev_index = dev['index']
-#         print('dev_index', dev_index)
-while(True):
+        with sr.Microphone(device_index=dev_index) as source:
 
-    r = sr.Recognizer()
-    print(1)
+            audio = r.listen(source)
+            print(2)
+            MyText = r.recognize_google(audio, language = 'en-IN', show_all = True)
+            print(3)
+            text = MyText.get('alternative')
+            a = text[0]
+            recognizedVoice = a['transcript']
+            print(recognizedVoice)
+
+            if 'chandan' in recognizedVoice:
+                markAttendence(message,chatbutton,textarea,sendbutton)
+            else:
+                pass
 
 
-    with sr.Microphone(device_index=1) as source2:
-
-        audio2 = r.listen(source2)
-        print(2)
-        MyText = r.recognize_google(audio2, language = 'en-IN', show_all = True)
-        print(3)
-        text = MyText.get('alternative')
-        a = text[0]
-
-        print(a['transcript'])
